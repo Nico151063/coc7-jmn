ba6e6e6e786af41fd8dfaa570e767b2c2db53158
YopiYop <34473019+Nico151063@users.noreply.github.com>
2023-09-01 16:20:26 +0200

    correction fond fenêtre objett

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 # coc7-jmn
-Ce module force la modification de certaines fonctions du "System Foundry VTT "Cthulhu V7"
+Ce module force la modification de certaines fonctions du "System Foundry VTT "Cthulhu V7".
+___** Mis à jour pour la Version 11 de FoundryVTT **___
 
+---
 # CE MODULE EST DESTINE A UN USAGE PRIVE
+---
 
-A ce jour, les modifications actives :
+_A ce jour, les modifications actives :_
 - Suppression des images de fond pour les points de vie et SAN sur la feuille de personnage.
-- Ajout possibilité de modifier l'icône de status DEAD
+- Ajout possibilité de modifier l'icône de status DEAD.
 
+_Fonctionnalités supplémentaires :_
+- Ajout d'une Macro pour la gestion de l'éclairage pour les Tokens.
+
+---
 ## Attributions
 Icons from game-icons.net  
--- scripts/util.js
+++ scripts/util.js
@@ -1,4 +1,4 @@
-import { MODULE,DEATH_ICON_FILENAME } from "./const.js"
+import { MODULE, DEATH_ICON_FILENAME } from "./const.js"
 import { registerSettings } from "./settings.js"
 
 
@@ -6,12 +6,12 @@ import { registerSettings } from "./settings.js"
 // Modification icone DEAD
 export async function updateDeathIcon(iconFileName) {
   console.log("---------- updateDeathIcon");
-  
+
   // Recherche Index Icon
   let effectIndex = CONFIG.statusEffects.findIndex(t => t.id === 'dead');
 
   // Modification Icon DEAD
-   if ((effectIndex !== -1) && (iconFileName !== "")) {
+  if ((effectIndex !== -1) && (iconFileName !== "")) {
     CONFIG.statusEffects[effectIndex].icon = iconFileName;
   }
   console.log("---------- *updateDeathIcon");
@@ -20,17 +20,18 @@ export async function updateDeathIcon(iconFileName) {
 
 // FORCAGE Options Ruleset
 export async function updateRulesetOptions() {
-  console.log("---------- updateRulesetOptions"); 
+  console.log("---------- updateRulesetOptions");
   
-  game.settings.set('CoC7', 'artworkSheetImage', "null");                     //url('./assets/images/tentacles.webp')
-  game.settings.set('CoC7', 'artWorkSheetBackground', "null");                //url('./assets/images/background.webp') 4 repeat
-  game.settings.set('CoC7', 'artWorkOtherSheetBackground', "null");           //url( './assets/images/background.webp')
-
-/*
-  game.settings.set('CoC7', 'artworkFrontColor', "rgba(127,0,0,1)");          //rgba(43,55,83,1)
-  game.settings.set('CoC7', 'artworkBackgroundColor', "rgba(127,0,0,1)");     //rgba(43,55,83,1)
-  game.settings.set('CoC7', 'artworkInteractiveColor', "rgba(127,0,0,1)");    //rgba(103,11,11,1)
-*/
+  if (game.settings.get('CoC7', 'overrideSheetArtwork')) {
+    game.settings.set('CoC7', 'artworkSheetImage', "null");                     //url('./assets/images/tentacles.webp')
+    game.settings.set('CoC7', 'artWorkSheetBackground', "null");                //url('./assets/images/background.webp') 4 repeat
+    //game.settings.set('CoC7', 'artWorkOtherSheetBackground', "null");           //url( './assets/images/background.webp')
+    /*
+    game.settings.set('CoC7', 'artworkFrontColor', "rgba(127,0,0,1)");          //rgba(43,55,83,1)
+    game.settings.set('CoC7', 'artworkBackgroundColor', "rgba(127,0,0,1)");     //rgba(43,55,83,1)
+    game.settings.set('CoC7', 'artworkInteractiveColor', "rgba(127,0,0,1)");    //rgba(103,11,11,1)
+  */
+  }
 
   console.log("---------- *updateRulesetOptions");
 }
